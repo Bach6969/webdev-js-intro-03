9b42eeb584f56e5b59f6916e899470dbee61c1e1
bach <bachlynn23@gmail.com>
2024-12-26 17:40:40 -0500

    updated assignment per request

--- js/index.js
+++ js/index.js
@@ -2,3 +2,19 @@
 // that are not declared
 "use strict"
 
+/*
+console.log(5+5);
+console.log(10-5);
+console.log(5 < 10 && 10 < 15); 
+console.log(5 > 10 || 10 < 15);
+
+let str1 = "apple";
+let str2 = "banana";
+
+console.log(str1 < str2); // Output: true (lexicographical order)
+
+let num = 10;
+let result = (num % 2 === 0) ? "Even" : "Odd";
+
+console.log(result); // Output: Even
+*/
